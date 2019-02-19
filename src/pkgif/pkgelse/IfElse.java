f967478427758025a71d18dc07c1a45cea5f198c
sanduni-E <sanduniue@gmail.com>
2019-02-19 20:40:39 +0530

    If-Else
    If-Else

---
+++ src/pkgif/pkgelse/IfElse.java
@@ -0,0 +1,18 @@
+
+package pkgif.pkgelse;
+
+
+public class IfElse {
+
+   
+    public static void main(String[] args) {
+       String name="Bee";
+       if(name=="Dee"){
+           System.out.println("the name is Dee");
+       }
+       else{
+           System.out.println("the name is Bee")
+       }
+    }
+    
+}
